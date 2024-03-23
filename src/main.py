b67d96a32862635ee4c0a0fa8d497be1c8667eef
BB <biring@gmail.com>
2024-03-23 00:43:07 -0400

    Added 'user selection' function
    1. generic function in console.py for user to select from a list
    
    2. main menu user selection

--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
 import application
+import console
 
 
 def main():
-    application.ebom_scrub_for_stella()
-    # application.cbom_scrub_for_cost_walk()
-    print("We are done. Exiting application")
-    print()
+    try:
+        # list of main menu option
+        menu_options = ['eBOM scrub for stella', 'cBOM scrub for cost walk']
+        # get user to make a selection
+        msg = "*** MAIN MENU ***"
+        user_selection = console.get_user_selection(menu_options, header_msg=msg)
+        # run user selection
+        if user_selection == 0:
+            application.ebom_scrub_for_stella()
+        elif user_selection == 1:
+            application.cbom_scrub_for_cost_walk()
+        else:
+            print("Invalid selection. Please choose a valid option.")
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        # Regardless of whether an exception occurred or not, print this message.
+        print("We are done. Exiting application.")
+        print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
