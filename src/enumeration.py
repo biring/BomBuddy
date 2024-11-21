2b704e3131aee74589f6e3e791ce3d23eb3b2575
BB <biring@gmail.com>
2024-11-21 15:36:48 -0500

    Refactored enum class to align more in line with intended use

--- src/enumeration.py
+++ src/enumeration.py
@@ -1,25 +1,28 @@
 from enum import Enum
 
-class BomType(Enum):
-    CW = "CW"  # Cost Walk BOM
-    CBOM = "CBOM"  # Component BOM
-    EBOM = "EBOM"  # Engineering BOM
-
-    def __str__(self):
-        return self.value  # So it will return the string representation when used
-
-class FilePrefix(Enum):
-    CW = "COST_WALK "
-    CBOM = "CBOM FOR DB "
-    EBOM = "EBOM FOE DB "
-
-    def __str__(self):
-        return self.value # So it will return the string representation when used
-
-class BomTemplateVersion(Enum):
-    v1 = "version 1"
+class BomTempVer(Enum):
+    v1 = "version 1" # Default not supported
     v2 = "version 2"
     v3 = "version 3"
 
     def __str__(self):
         return self.value # So it will return the string representation when used
+
+class SourceFileType(Enum):
+    NA = "N/A"  # Default not supported
+    CB = "CB"  # Costed BOM
+    EB = "EB"  # Engineering BOM
+
+    def __str__(self):
+        return self.value # So it will return the string representation when used
+
+
+class OutputFileType(Enum):
+    NA = "NA "  # Default not supported
+    CW = "CW "  # Cost Walk
+    dB_CB = "dB_CBOM "  # CBOM for dB upload
+    db_EB = "dB_EBOM "  # EBOM for dB uploadEngineering BOM
+
+    def __str__(self):
+        return self.value  # So it will return the string representation when used
+
